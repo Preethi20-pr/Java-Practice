b0963fbbb1b806e55c8b053ec74e086f9ba9f402
Preethi20-pr <80383424+Preethi20-pr@users.noreply.github.com>
2023-02-07 20:45:26 +0530

    java practice

---
+++ greatest3.java
@@ -0,0 +1,8 @@
+class greatest3{
+public static void main(String args[])
+{
+int num1=12,num2=7,num3=14,res;
+res=num3>(num1<num2?num1:num2)?num3:(num1<num2?num1:num2);
+System.out.println("the biggest of 3 is"+res);
+}
+}
