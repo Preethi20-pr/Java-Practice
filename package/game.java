aae1193d95294495ba4b3c526c5c1e83154b6db8
Preethi20-pr <80383424+Preethi20-pr@users.noreply.github.com>
2023-03-02 21:48:03 +0530

    2/03/23

---
+++ package/game.java
@@ -0,0 +1,8 @@
+package mypack;
+public class game
+{
+  public void play()
+  {
+   System.out.println("You have win the game");
+   }
+}
---
+++ package/win.java
@@ -0,0 +1,8 @@
+package start;
+import mypack.*;
+public class win{
+  public static void main(String args[]){
+     game g1=new game();
+    g1.play();
+   }
+}
